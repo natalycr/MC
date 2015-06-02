a847194cd19882dda2bf0992694decde7cff6fea
natalycr <natyCR27@Natalys-MacBook-Pro.local>
2015-06-02 01:23:58 -0500

    Martes

---
+++ Tarea/HW1/bruno.sh
@@ -0,0 +1,25 @@
+#! /bin/ bash
+
+clear
+
+# importa  los datos
+curl https://raw.githubusercontent.com/ComputoCienciasUniandes/MetodosComputacionales/master/homework/2015-V/HW1/kepler.csv > header.csv 
+
+# quita la primera linea
+sed '1d' header.csv > planetas.csv
+
+rm header.csv # elimina el archivo con la primera fila
+
+ 
+# Punto (a)
+
+echo "Número de planetas extrasolares en el catálogo: $(wc -l planetas.csv) "
+
+#Punto (b)
+
+declare massJupiter = 1.899 # por 10 ^27 Kg OJO con las unidades!!!!
+
+planetas.csv > awk '{if($10< ((massJupiter/10))){print $1}' > planetas1_10.csv
+
+echo "Número de planetas extrasolares con 1/10 la masa de Jupiter: $(wc -l planetas1_10.csv) "
+
